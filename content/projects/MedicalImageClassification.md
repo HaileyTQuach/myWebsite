90edfd8922b1929917c2e986d680dfe867a0b048
HaileyQ <thaoqp.hailey@gmail.com>
2024-06-16 16:22:47 -0400

    Finished final touches

---
+++ content/projects/MedicalImageClassification.md
@@ -0,0 +1,15 @@
+---
+date: '2023-12-01'
+title: 'Medical Image Classification Using CNN'
+github: 'https://github.com/HaileyTQuach/Medical-Image-Classification-using-CNN'
+external: ''
+tech:
+  - Deep Learning/ Convolutional Neural Networks
+  - PyTorch
+  - Python Libraries
+  - Scikit-Learn
+company: 'Concordia University - COMP432'
+showInProjects: true
+---
+
+Applying CNN to learn and extract features from medical images
--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,9 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I am seeking internship opportunities for Fall 2024 and Winter 2025. Feel free to reach out
+        to me anytime. Whether you have a question or just want to say hi, I’ll try my best to get
+        back to you!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
